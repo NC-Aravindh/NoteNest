63dd6cfbd925b2aab4ec975386097660211038bb
Aravindh.NC <aravindhnc@gmail.com>
2024-11-21 20:31:21 +0530

    minor corrections

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -1,16 +1,14 @@
 import DeleteIcon from "@mui/icons-material/Delete";
-function Note({id, title, content , onDelete}) {
+function Note({ id, title, content, onDelete }) {
   //passing the id of the note item that was deleted.
   function sendNoteId() {
     onDelete(id);
   }
 
   return (
-    // contentEditable="true"
     <div className="note-container">
-      <h1 >{title}</h1>
-      {/* <p contentEditable="true">{content}</p> */}
-      <p >{content}</p>
+      <h1>{title}</h1>
+      <p>{content}</p>
       <DeleteIcon id="delete-icon" onClick={sendNoteId} />
     </div>
   );
